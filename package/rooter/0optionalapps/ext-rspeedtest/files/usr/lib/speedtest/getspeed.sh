4e55343f5504c44377ecb26797166ffa2b397c84
Dairyman <ralph@rfactory.org>
2024-04-07 08:09:35 -0700

    speedtest
    speedtest

--- package/rooter/0optionalapps/ext-rspeedtest/files/usr/lib/speedtest/getspeed.sh
+++ package/rooter/0optionalapps/ext-rspeedtest/files/usr/lib/speedtest/getspeed.sh
@@ -32,7 +32,7 @@ url=${line:7:100}
 url=$(echo $url" " | tr "/" ",")
 url=$(echo "$url" | cut -d, -f1)
 rm -f /tmp/speed
-speedtest --test-server $url --output text > /tmp/speed &
+speedtest --list-server --output text > /tmp/speed &
 while [ -z "$JITTER" ]
 do
 	if [ -e /tmp/speed ]; then
